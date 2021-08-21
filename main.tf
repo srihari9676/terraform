2d49ca000b40112851a0329d5796af9816500d59
srihari9676 <84854762+srihari9676@users.noreply.github.com>
2021-08-21 21:01:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,31 @@
+provider "sendgrid" {
+  api_key = "SG.abc123"
+}
 
+resource "sendgrid_subuser" "user1" {
+  username = "subuser1"
+  email    = "isrihari258@gmail.com"
+
+}
+
+password {
+    destination = "./output/user1.pass"
+
+    ips = [
+    "255.255.255.255"
+  ]
+}
+
+resource "sendgrid_api_key" "user1" {
+  name = "my-account-subuser1"
+
+}
+
+scopes = [
+    "mail.batch.create",
+    "mail.batch.delete",
+    "mail.batch.read",
+    "mail.batch.update",
+    "mail.send"
+  ]
+}
